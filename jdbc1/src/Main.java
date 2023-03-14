595528f954f886a7cb73b55f5d6354e1b44298b7
Vitor Pereira <vitorpereiraresende26@gmail.com>
2023-03-14 10:38:35 -0300

    inicializing sql connection and first data recuperation with jdbc

---
+++ jdbc1/src/Main.java
@@ -0,0 +1,12 @@
+import db.DB;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+        System.out.println("Done!");
+    }
+}
